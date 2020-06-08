4a8a8e503044dc6f2c2ae35d850b6dd478cb2b4c
Enzo <enzoap2009@hotmail.com>
2020-06-08 18:14:12 -0300

    Finalizado o app

--- web/src/pages/CreatePoints/index.tsx
+++ web/src/pages/CreatePoints/index.tsx
@@ -6,6 +6,8 @@ import { LeafletMouseEvent } from "leaflet";
 import axios from 'axios'
 import api from '../../services/api';
 
+import Dropzone from '../../components/Dropzone/dropzone'
+
 import './styles.css'
 import logo from '../../assets/logo.svg'
 
@@ -38,7 +40,9 @@ const CreatePoint = () => {
         whatsapp: ""
     })
     const [selectedItems, setSelectedItems] = useState<number[]>([])
+    const [selectedFile, setSelectedFile] = useState<File>()
     //array ou objeto, manualmente informar o tipo da variavel
+
     const history = useHistory()
     useEffect(() => {
         api.get('items').then(response => {
@@ -113,6 +117,7 @@ const CreatePoint = () => {
     }
 
     async function handleSubmit(event: FormEvent){
+
         event.preventDefault()
 
         const {name, email, whatsapp} = formData
@@ -121,15 +126,19 @@ const CreatePoint = () => {
         const [latitude, longitude] = selectedPosition
         const items = selectedItems
 
-        const data = {
-            name,
-            email,
-            whatsapp,
-            uf,
-            city,
-            latitude,
-            longitude,
-            items
+        const data = new FormData()
+
+        data.append('name',name)
+        data.append('email',email)
+        data.append('whatsapp',whatsapp)
+        data.append('uf',uf)
+        data.append('city',city)
+        data.append('latitude',String(latitude))
+        data.append('longitude',String(longitude))
+        data.append('items',items.join(','))
+
+        if(selectedFile){
+            data.append('image', selectedFile)
         }
 
         await api.post('points', data)
@@ -152,6 +161,7 @@ const CreatePoint = () => {
 
             <form onSubmit={handleSubmit}>
                 <h1>Cadastro do <br/> ponto de coleta</h1>
+                <Dropzone onFileUploaded={setSelectedFile}/>
                 <fieldset>
                     <legend>
                         <h2>Dados</h2>
@@ -172,8 +182,6 @@ const CreatePoint = () => {
                             <input onChange={handleInputChange} type="text" name="whatsapp" id="whatsapp"/>
                         </div>
                     </div>
-
-                    
                 </fieldset>
 
                 <fieldset>
